4f0a0724372c9d3431ae1e0bef585512b86a75c0
eespee <paulsendroiu@gmail.com>
2022-02-06 22:21:12 +0200

    changes

--- Assets/ShakerController.cs
+++ Assets/ShakerController.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class ShakerController : MonoBehaviour
 {
     Animator animator;
-    GameObject Lid;
+    public GameObject Lid;
+    public GameObject OpaqueBody;
+    public GameObject TransparentBody;
+
     private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetTransparent(bool t)
+    {
+        OpaqueBody.SetActive(!t);
+        Lid.SetActive(!t);
+        TransparentBody.SetActive(t);
     }
 }
